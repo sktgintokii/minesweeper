4b042135f40ab2f8e17998eadc46ea32c08e4c5c
FelixYau <sktgintokii@gmail.com>
2019-09-21 16:54:57 +0800

    Updates

---
+++ precache-manifest.7878c2d653f40b869d2ad80239c72d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eec20a1c56c80229c91338dfa504a3c",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "9fc0858f35840449841e",
+    "url": "/minesweeper/static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "e2b8bbafff2f1c59e8d9",
+    "url": "/minesweeper/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9fc0858f35840449841e",
+    "url": "/minesweeper/static/js/2.8ce5d664.chunk.js"
+  },
+  {
+    "revision": "e2b8bbafff2f1c59e8d9",
+    "url": "/minesweeper/static/js/main.c62e63fd.chunk.js"
+  },
+  {
+    "revision": "5c7044c493615d2d6941",
+    "url": "/minesweeper/static/js/runtime-main.59bc03dc.js"
+  }
+]);
